cabedc9fa390833b8da89faa336276c58220ce09
groos <ntgroos@gmail.com>
2014-10-31 10:41:24 -0500

    Added credentials, extra column for user name.

--- tweet2csv.py
+++ tweet2csv.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
-import argparse, csv, codecs, cStringIO, sys, tweepy
+import twitter, argparse, csv, codecs, cStringIO, sys, tweepy
+
 
 # From: http://docs.python.org/library/csv.html
 class UnicodeWriter:
@@ -18,7 +19,7 @@ class UnicodeWriter:
 
     def writerow(self, row):
         # Some s in row might not have encode method!!!
-        self.writer.writerow([s.encode("utf-8") if hasattr(s, 'encode') else s for s in row])
+        self.writer.writerow([s.encode("utf-8") if hasattr(s, 'encode') else s for s in row]) #list comprehension
         # Fetch UTF-8 output from the queue ...
         data = self.queue.getvalue()
         data = data.decode("utf-8")
@@ -26,6 +27,7 @@ class UnicodeWriter:
         data = self.encoder.encode(data)
         # write to the target stream
         self.stream.write(data)
+        
         # empty queue
         self.queue.truncate(0)
 
@@ -33,9 +35,16 @@ class UnicodeWriter:
         for row in rows:
             self.writerow(row)
 
+
+#creds
+auth = tweepy.OAuthHandler('your_creds', 
+							'your_creds')
+auth.set_access_token('your_creds', 
+						'your_creds')
+
 parser = argparse.ArgumentParser(description="Query the Twitter search API and output results as csv")
 parser.add_argument('query', metavar='QUERY', help="Query to send to Twitter search API. See https://dev.twitter.com/docs/using-search for example queries")
-parser.add_argument('-c', '--columns', nargs='+', default=['id_str', 'from_user', 'created_at', 'text'], help="Columns to display")
+parser.add_argument('-c', '--columns', nargs='+', default=['id_str', 'author', 'created_at', 'text'], help="Columns to display")
 
 csv_group = parser.add_argument_group('CSV options', 'Options for creating the CSV file')
 csv_group.add_argument('-d', '--delimiter', default=',')
@@ -45,13 +54,24 @@ options = parser.parse_args()
 
 out_csv = UnicodeWriter(sys.stdout, delimiter=options.delimiter.decode('string_escape'), lineterminator=options.line_terminator.decode('string_escape'))
 
-out_csv.writerow(options.columns)
+out_csv.writerow(['name', 'id_str', 'author_object', 'created_at', 'text'])
 
-api = tweepy.API()
+api = tweepy.API(auth)
 # tweepy.Cursor = pagination
 for tweet in tweepy.Cursor(api.search, q=options.query, rpp=100, result_type='recent').items():
     try:
-        out_csv.writerow([getattr(tweet, column) for column in options.columns])
+    	'''
+    	Going to build each row, with author.user.name inserted at beginning.
+    	'''
+    	attrs = []
+    	author = getattr(tweet, 'author')
+    	attrs.append(author.name)
+    	
+    	for column in options.columns:
+    		attrs.append(getattr(tweet, column))
+    	
+    	out_csv.writerow([attr for attr in attrs])
+    	
     except AttributeError as e:
         sys.stderr.write(str(e))
         sys.exit(1)
